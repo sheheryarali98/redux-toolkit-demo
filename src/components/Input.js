2639180046d070bfeb801709b4663d2c7443910b
sheheryarali98 <ssssyed98@gmail.com>
2023-01-27 12:49:38 +0500

    Integrated formik and added form validation

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,15 +1,52 @@
 import PropTypes from 'prop-types';
 
-const Input = ({ className }) => (
-  <input className={`p-2 border border-gray-400 rounded ${className}`} />
-);
+const Input = ({
+  className,
+  name,
+  value,
+  onChange,
+  hasError,
+  errorMessage,
+}) => {
+  const fieldHasError = !!(hasError && errorMessage);
+
+  const getBorder = () => (fieldHasError ? 'border-2' : 'border');
+
+  const getBorderColor = () => (fieldHasError ? 'border-red-500' : 'border-gray-400');
+
+  return (
+    <div className={`flex flex-col justify-center items-start ${className}`}>
+      <input
+        name={name}
+        value={value}
+        onChange={onChange}
+        className={`p-2 ${getBorder()} ${getBorderColor()} rounded w-full`}
+      />
+      {fieldHasError && (
+        <div className="text-red-500 text-sm font-medium mt-1">
+          {errorMessage}
+        </div>
+      )}
+    </div>
+  );
+};
 
 Input.propTypes = {
   className: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  hasError: PropTypes.bool,
+  errorMessage: PropTypes.string,
 };
 
 Input.defaultProps = {
   className: null,
+  name: '',
+  value: '',
+  onChange: () => {},
+  hasError: false,
+  errorMessage: '',
 };
 
 export default Input;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,14 +1,42 @@
 import PropTypes from 'prop-types';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import Button from './Button';
 import Input from './Input';
 
-const TodoForm = ({ className }) => (
-  <form className={`flex justify-start items-center gap-4 ${className}`}>
-    <Input className="grow" />
-    <Button type="submit" className="w-1/5">Add</Button>
-  </form>
-);
+const TodoForm = ({ className }) => {
+  const {
+    values,
+    handleChange,
+    handleSubmit,
+    errors,
+  } = useFormik({
+    initialValues: {
+      title: '',
+    },
+    onSubmit: ({ title }) => {
+      console.log(title);
+    },
+    validationSchema: Yup.object().shape({
+      title: Yup.string().required('Title is required'),
+    }),
+  });
+
+  return (
+    <form onSubmit={handleSubmit} className={`flex justify-start items-start gap-4 ${className}`}>
+      <Input
+        name="title"
+        value={values.title}
+        onChange={handleChange}
+        hasError={!!errors.title}
+        errorMessage={errors.title}
+        className="grow"
+      />
+      <Button type="submit" className="w-1/5">Add</Button>
+    </form>
+  );
+};
 
 TodoForm.propTypes = {
   className: PropTypes.string,
